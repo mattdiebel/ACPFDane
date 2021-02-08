75740a8aca78edbb86de3b716600a02b25b28923
mattdiebel <mwdiebel@gmail.com>
2021-02-08 16:25:27 -0600

    Update unit_stream_power.R

--- unit_stream_power.R
+++ unit_stream_power.R
@@ -1,127 +1,118 @@
-  ########## Specify geodatabase path!!! ##########
-  setwd("acpf.gdb")
+setwd("acpf.gdb")
+source("ManningWidth.R")
 
-  library(arcgisbinding)
-  arc.check_product()
-  
-  source("C:/LWRD/Yahara_CLEAN/flow_paths/ManningWidth.R")
-  
-  # in_param_flowPaths = in_params[[1]]
-  # in_param_transectPoints = in_params[[2]]
-  # 
-  # out_param_flowPaths = out_params[[1]]
-  # out_param_transects = out_params[[2]]
-  
-  in_param_flowPaths = 'flowPaths'
-  in_param_transectPoints = 'transectPoints2'
+library(arcgisbinding)
+arc.check_product()
 
-  out_param_flowPaths = 'flowPathsUSP'
-  out_param_transects = 'transectsUSP'
-  
-  flowPathsFC = arc.open(in_param_flowPaths)
-  flowPaths = arc.select(flowPathsFC)
-  flowPaths$n = NA
-  flowPaths$USP = NA
-  flowPaths$RORUS[is.na(flowPaths$RORUS)] = 0
-  
-  transectPoints = arc.open(in_param_transectPoints)
-  data = arc.select(transectPoints)
-  
-  for (p in 1:nrow(flowPaths)) {
-    flowPaths$n[p] = length(unique(data$transectID[data$pathID==flowPaths$OBJECTID[p]]))
-    if (is.na(flowPaths$FlowAcc[p])) {
-      flowPaths$FlowAcc[p] = sum(flowPaths$FlowAcc[flowPaths$TO_ID==flowPaths$FROM_ID[p]], na.rm = TRUE)
-    }
+in_param_flowPaths = 'flowPaths'
+in_param_transectPoints = 'transectPoints2'
+out_param_flowPaths = 'flowPathsUSP'
+out_param_transects = 'transectsUSP'
+
+flowPathsFC = arc.open(in_param_flowPaths)
+flowPaths = arc.select(flowPathsFC)
+flowPaths$n = NA
+flowPaths$USP = NA
+flowPaths$RORUS[is.na(flowPaths$RORUS)] = 0
+
+transectPoints = arc.open(in_param_transectPoints)
+data = arc.select(transectPoints)
+
+for (p in 1:nrow(flowPaths)) {
+  flowPaths$n[p] = length(unique(data$transectID[data$pathID==flowPaths$OBJECTID[p]]))
+  if (is.na(flowPaths$FlowAcc[p])) {
+    flowPaths$FlowAcc[p] = sum(flowPaths$FlowAcc[flowPaths$TO_ID==flowPaths$FROM_ID[p]], na.rm = TRUE)
   }
-  
-  all_transects = data.frame()
-  
-  for (p in 1:nrow(flowPaths)) {
-    remainder = flowPaths$LengthM[p] - (flowPaths$n[p]-2)*100
-    pdata = data[data$pathID==flowPaths$OBJECTID[p],]
-    transects = data.frame(pathID = flowPaths$OBJECTID[p],
-                           transectID = unique(pdata$transectID),
-                           order = NA,
-                           Elevation = NA,
-                           FlowAcc = flowPaths$FlowAcc[p],
-                           RORUS = flowPaths$RORUS[p],
-                           Qd = NA,
-                           S = NA,
-                           W = NA,
-                           L = 100)
-    
-    # Sort transects downstream (Arc tool does the end transects last (e.g., 2,3,4,1,5))
-    transects$order = rank(transects$transectID, ties.method = "first")
-    transects = transects[order(transects$order),]
-    transects$order[nrow(transects)-1] = 0
-    transects = transects[order(transects$order),]
-    
-    # Copy channel elevation to transects
-    for (t in 1:nrow(transects)) {
-      tdata = data[data$transectID==transects$transectID[t],]
-      tdata = tdata[order(tdata$pointID),]
-      transects$Elevation[t] = tdata$Elevation[9]
+}
+
+all_transects = data.frame()
+
+for (p in 1:nrow(flowPaths)) {
+  remainder = flowPaths$LengthM[p] - (flowPaths$n[p]-2)*100
+  pdata = data[data$pathID==flowPaths$OBJECTID[p],]
+  transects = data.frame(pathID = flowPaths$OBJECTID[p],
+                         transectID = unique(pdata$transectID),
+                         order = NA,
+                         Elevation = NA,
+                         FlowAcc = flowPaths$FlowAcc[p],
+                         RORUS = flowPaths$RORUS[p],
+                         Qd = NA,
+                         S = NA,
+                         W = NA,
+                         L = 100)
+
+  # Sort transects downstream (Arc tool does the end transects last (e.g., 2,3,4,1,5))
+  transects$order = rank(transects$transectID, ties.method = "first")
+  transects = transects[order(transects$order),]
+  transects$order[nrow(transects)-1] = 0
+  transects = transects[order(transects$order),]
+
+  # Copy channel elevation to transects
+  for (t in 1:nrow(transects)) {
+    tdata = data[data$transectID==transects$transectID[t],]
+    tdata = tdata[order(tdata$pointID),]
+    transects$Elevation[t] = tdata$Elevation[9]
+  }
+
+  # Calculate wetted width for each transect
+  for (t in 1:(nrow(transects))) {
+    if(flowPaths$n[p]==2) {
+      A = transects$FlowAcc[1] * 4 / 2590000 * transects$RORUS[1]
+      if (t==1) {
+        transects$S[t] = (transects$Elevation[t] - transects$Elevation[t+1]) / (100 * remainder)
+        transects$L[t] = remainder/2
+      } else {
+        transects$S[t] = (transects$Elevation[t-1] - transects$Elevation[t]) / (100 * remainder)
+        transects$L[t] = remainder/2
+      }
     }
-    
-    # Calculate wetted width for each transect
-    for (t in 1:(nrow(transects))) {
-      if(flowPaths$n[p]==2) {
+
+    if(flowPaths$n[p]>2) {
+      if (t==1) {
         A = transects$FlowAcc[1] * 4 / 2590000 * transects$RORUS[1]
-        if (t==1) {
-          transects$S[t] = (transects$Elevation[t] - transects$Elevation[t+1]) / (100 * remainder)
-          transects$L[t] = remainder/2
-        } else {
-          transects$S[t] = (transects$Elevation[t-1] - transects$Elevation[t]) / (100 * remainder)
-          transects$L[t] = remainder/2
-        }
+        transects$S[t] = (transects$Elevation[t] - transects$Elevation[t+1]) / 10000
       }
-      
-      if(flowPaths$n[p]>2) {
-        if (t==1) {
-          A = transects$FlowAcc[1] * 4 / 2590000 * transects$RORUS[1]
-          transects$S[t] = (transects$Elevation[t] - transects$Elevation[t+1]) / 10000
-        }
-        if (t>1 & t<flowPaths$n[p]) {
-          A = transects$FlowAcc[t] * 4 / 2590000 * transects$RORUS[t]
-          transects$S[t] = (transects$Elevation[t-1] - transects$Elevation[t]) / 10000
-        }
-        if (t==flowPaths$n[p]) {
-          A = transects$FlowAcc[t-1] * 4 / 2590000 * transects$RORUS[t-1]
-          transects$S[t] = (transects$Elevation[t-1] - transects$Elevation[t]) / (100 * remainder)
-          transects$L[t] = remainder
-        }
+      if (t>1 & t<flowPaths$n[p]) {
+        A = transects$FlowAcc[t] * 4 / 2590000 * transects$RORUS[t]
+        transects$S[t] = (transects$Elevation[t-1] - transects$Elevation[t]) / 10000
       }
-      
-      if (transects$S[t]<=0) {transects$S[t] = 0.0001}
-      transects$Qd[t] = 35 * A^0.649 
-      tdata = data[data$transectID==transects$transectID[t],]
-      tdata = tdata[order(tdata$pointID),]
-      tdata = tdata[1:17,]
-      tdata$X = c(0:16*2)
-      tdata$Y = tdata$Elevation/100
-      tdata = tdata[,c("X","Y")]
-      transects$W[t] = ManningWidth(tdata, transects$Qd[t], transects$S[t], 0.04)
+      if (t==flowPaths$n[p]) {
+        A = transects$FlowAcc[t-1] * 4 / 2590000 * transects$RORUS[t-1]
+        transects$S[t] = (transects$Elevation[t-1] - transects$Elevation[t]) / (100 * remainder)
+        transects$L[t] = remainder
+      }
     }
-  
-    # Calculate unit stream power
-    transects = transects[!is.na(transects$W),]
-    transects$USP = (1000 * 9.8 * transects$Qd/3.28084^3 * transects$S) / (transects$W/3.28084)
-    all_transects = rbind(all_transects, transects)
-    
-    pct = round(p/nrow(flowPaths),3)
-    print(pct)
-    flush.console()
-  
+
+    if (transects$S[t]<=0) {transects$S[t] = 0.0001}
+    transects$Qd[t] = 35 * A^0.649 
+    tdata = data[data$transectID==transects$transectID[t],]
+    tdata = tdata[order(tdata$pointID),]
+    tdata = tdata[1:17,]
+    tdata$X = c(0:16*2)
+    tdata$Y = tdata$Elevation/100
+    tdata = tdata[,c("X","Y")]
+    transects$W[t] = ManningWidth(tdata, transects$Qd[t], transects$S[t], 0.04)
   }
-  
-  all_transects$USPL = all_transects$USP * all_transects$L
-  for (p in 1:nrow(flowPaths)) {
-    pt = all_transects[all_transects$pathID==flowPaths$OBJECTID[p],]
-    flowPaths$USP[p] = sum(pt$USPL, na.rm = TRUE) / sum(pt$L, na.rm = TRUE)
-  }
-  
-  arc.write(out_param_transects, all_transects, overwrite = TRUE)
-  arc.write(out_param_flowPaths, flowPaths, overwrite = TRUE)
+
+  # Calculate unit stream power
+  transects = transects[!is.na(transects$W),]
+  transects$USP = (1000 * 9.8 * transects$Qd/3.28084^3 * transects$S) / (transects$W/3.28084)
+  all_transects = rbind(all_transects, transects)
+
+  pct = round(p/nrow(flowPaths),3)
+  print(pct)
+  flush.console()
+
+}
+
+all_transects$USPL = all_transects$USP * all_transects$L
+for (p in 1:nrow(flowPaths)) {
+  pt = all_transects[all_transects$pathID==flowPaths$OBJECTID[p],]
+  flowPaths$USP[p] = sum(pt$USPL, na.rm = TRUE) / sum(pt$L, na.rm = TRUE)
+}
+
+arc.write(out_param_transects, all_transects, overwrite = TRUE)
+arc.write(out_param_flowPaths, flowPaths, overwrite = TRUE)
   
 
 # Test arguments for ManningWidth function
